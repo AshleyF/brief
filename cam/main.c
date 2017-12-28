a790661f1158bc4d96c3f045d0e217bf6dfc5c0a
Ashley Nathan Feniello <ashley.feniello@live.com>
2017-12-28 11:56:33 -0800

    Simple web page displaying camera frames

--- cam/main.c
+++ cam/main.c
@@ -1,23 +1,146 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include "camera.h"
 #include "server.h"
 
-void process(int conn, unsigned char* frame, int length) {
+// abc\r\nde -> abc\0 + de
+// de (shift, read)
+// def\r\n\ghir\n\r\n
+// abc\r\ndef\r\n\r\n
+
+const int MAXLINE = 1024;
+
+int len = 0;
+unsigned char* next;
+
+unsigned char* readLine(int fd, unsigned char* buf)
+{
+	// printf("PARSE LINE\n");
+	if (len <= 0)
+	{
+		// fill buffer
+		// printf("  READ\n");
+		// printf("  BUFFER0: %s\n", buf);
+		len = read(fd, buf, MAXLINE);
+		if (len == 0)
+		{
+			perror("Unable to read from socket.");
+			exit(-1);
+		}
+		// printf("  BUFFER1 (LEN=%i): %s\n", len, buf);
+		next = buf;
+	}
+
+	// find terminator
+	// printf("  FIND TERMINATOR\n");
+	for (int i = 0; i < len; i++)
+	{
+		if (next[i] == '\r')
+		{
+			// found - replace with '\0' and update `next`/`len`
+			// printf("    FOUND\n");
+			next[i] = '\0';
+			unsigned char* line = next;
+			i += 2;
+			next += i;
+			len -= i;
+			return line;
+		}
+	}
+
+	if (len >= MAXLINE)
+	{
+		perror("HTTP header exceeds buffer or is underminated.");
+		exit(-1);
+	}
+
+	// printf("    NOT FOUND\n");
+	// not found - shift remaining data and fill remaining buffer
+	// printf("  SHIFT\n");
+	// printf("  OLD BUFFER: %s\n", buf);
+	for (int i = 0; i < len; i++)
+	{
+		//        de -> abc\0 + de
+		// de (shift, read)
+		buf[i] = next[i];
+	}
+	next = buf;
+	len += read(fd, buf + len, MAXLINE - len);
+	// printf("  NEW BUFFER: %s\n", buf);
+	// printf("  LEN=%i\n", len);
+	return readLine(fd, buf);
+}
+
+int startsWith(unsigned char* prefix, unsigned char* str)
+{
+	size_t len = strlen(prefix);
+	if (len > strlen(str))
+	{
+		printf("TOOLONG (%s=%ld, %s=%ld)\n", prefix, len, str, strlen(str));
+	}
+	printf("COMP '%s': %i (LEN=%ld)\n", prefix, strncmp(prefix, str, len), len);
+	return len > strlen(str) ? 0 : strncmp(prefix, str, len) == 0;
+}
+
+void process(int conn) {
     printf("Accept request, conn is %d\n", conn);
 
-	const int MAXLINE = 1024;
-    unsigned char buf[MAXLINE];
-	int foo = read(conn, buf, MAXLINE);
-	printf("TEST: %s", buf);
+	unsigned char buf[MAXLINE];
+	len = 0;
+	unsigned char* line = readLine(conn, buf);
+	printf("HEADER: %s\n", line);
+	if (startsWith("GET /frame.jpg", line))
+	{
+		printf("FRAME\n");
 
-    sprintf(buf, "HTTP/1.1 200 OK\r\n");
-    sprintf(buf + strlen(buf), "Content-Type: image/jpeg\r\n");
-    // sprintf(buf + strlen(buf), "Content-Length: %i\r\n", length);
-    sprintf(buf + strlen(buf), "\r\n");
-	int written = write(conn, buf, strlen(buf));
-    written = write(conn, frame, length);
+		unsigned char* frame;
+		int length = 0;
+		if (!camFrame(&frame, &length))
+		{
+			printf("Failed to get camera frame.\n");
+			sprintf(buf, "HTTP/1.1 500 Camera Failure\r\n");
+			sprintf(buf + strlen(buf), "\r\n");
+			write(conn, buf, strlen(buf));
+			return;
+		}
+		printf("Got frame! (len=%i)\n", length);
+
+		sprintf(buf, "HTTP/1.1 200 OK\r\n");
+		sprintf(buf + strlen(buf), "Content-Type: image/jpeg\r\n");
+		// sprintf(buf + strlen(buf), "Content-Length: %i\r\n", length);
+		sprintf(buf + strlen(buf), "\r\n");
+		write(conn, buf, strlen(buf));
+		write(conn, frame, length);
+	}
+	else if (startsWith("GET / ", line))
+	{
+		printf("PAGE\n");
+		sprintf(buf, "HTTP/1.1 200 OK\r\n");
+		sprintf(buf + strlen(buf), "Content-Type: text/html\r\n");
+		// sprintf(buf + strlen(buf), "Content-Length: %i\r\n", length);
+		sprintf(buf + strlen(buf), "\r\n");
+		sprintf(buf + strlen(buf), "<h1>Camera Test</h1><img onload='frame()' id='camera' /><script>\nvar camera = document.getElementById('camera');\nvar i = 0;\nfunction frame()\n{\ncamera.src = 'http://localhost/frame.jpg' + (i++);\n}\nframe();\n</script>");
+		write(conn, buf, strlen(buf));
+	}
+	else
+	{
+		printf("NOT FOUND\n");
+		sprintf(buf, "HTTP/1.1 404 Not Found\r\n");
+		sprintf(buf + strlen(buf), "\r\n");
+		write(conn, buf, strlen(buf));
+	}
+	/*
+	for (int i = 0; i < 20; i++)
+	{
+		unsigned char* line = readLine(conn, buf);
+		printf("LINE %i: %s (LEN=%i, %i)\n", i, line, len, line[0]);
+		if (line[0] == '\0') break;
+	}
+	printf("END\n");
+	*/
+
 }
 
 int main(int argc, char** argv)
@@ -31,24 +154,13 @@ int main(int argc, char** argv)
 	}
 
 	int fd = serverInit(80);
-	unsigned char* frame;
-	int length = 0;
     while(1)
 	{
         int conn = serverAccept(fd);
-
-		for (int i = 0; i < 8; i++) // driver seems to queue 8 frames
-		{
-			if (!camFrame(&frame, &length))
-			{
-				printf("Failed to get camera frame.\n");
-				return 1;
-			}
-		}
-		printf("Got frame! (len=%i)\n", length);
-
-		process(conn, frame, length);
+		printf("ACCEPTED CONNECTION: %i\n", conn);
+		process(conn);
 		serverClose(conn);
+		printf("CLOSED\n");
     }
 
     return 0;

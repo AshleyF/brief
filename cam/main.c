fc0517e0d543d52028cda1c5d499f883ed358b04
Ashley Nathan Feniello <ashley.feniello@live.com>
2017-12-28 17:59:27 -0800

    Removed debugging printfs

--- cam/main.c
+++ cam/main.c
@@ -5,11 +5,6 @@
 #include "camera.h"
 #include "server.h"
 
-// abc\r\nde -> abc\0 + de
-// de (shift, read)
-// def\r\n\ghir\n\r\n
-// abc\r\ndef\r\n\r\n
-
 const int MAXLINE = 1024;
 
 int len = 0;
@@ -17,30 +12,24 @@ unsigned char* next;
 
 unsigned char* readLine(int fd, unsigned char* buf)
 {
-	// printf("PARSE LINE\n");
 	if (len <= 0)
 	{
 		// fill buffer
-		// printf("  READ\n");
-		// printf("  BUFFER0: %s\n", buf);
 		len = read(fd, buf, MAXLINE);
 		if (len == 0)
 		{
 			perror("Unable to read from socket.");
 			exit(-1);
 		}
-		// printf("  BUFFER1 (LEN=%i): %s\n", len, buf);
 		next = buf;
 	}
 
 	// find terminator
-	// printf("  FIND TERMINATOR\n");
 	for (int i = 0; i < len; i++)
 	{
 		if (next[i] == '\r')
 		{
 			// found - replace with '\0' and update `next`/`len`
-			// printf("    FOUND\n");
 			next[i] = '\0';
 			unsigned char* line = next;
 			i += 2;
@@ -56,31 +45,19 @@ unsigned char* readLine(int fd, unsigned char* buf)
 		exit(-1);
 	}
 
-	// printf("    NOT FOUND\n");
 	// not found - shift remaining data and fill remaining buffer
-	// printf("  SHIFT\n");
-	// printf("  OLD BUFFER: %s\n", buf);
 	for (int i = 0; i < len; i++)
 	{
-		//        de -> abc\0 + de
-		// de (shift, read)
 		buf[i] = next[i];
 	}
 	next = buf;
 	len += read(fd, buf + len, MAXLINE - len);
-	// printf("  NEW BUFFER: %s\n", buf);
-	// printf("  LEN=%i\n", len);
 	return readLine(fd, buf);
 }
 
 int startsWith(unsigned char* prefix, unsigned char* str)
 {
 	size_t len = strlen(prefix);
-	if (len > strlen(str))
-	{
-		printf("TOOLONG (%s=%ld, %s=%ld)\n", prefix, len, str, strlen(str));
-	}
-	printf("COMP '%s': %i (LEN=%ld)\n", prefix, strncmp(prefix, str, len), len);
 	return len > strlen(str) ? 0 : strncmp(prefix, str, len) == 0;
 }
 
@@ -90,11 +67,9 @@ void process(int conn) {
 	unsigned char buf[MAXLINE];
 	len = 0;
 	unsigned char* line = readLine(conn, buf);
-	printf("HEADER: %s\n", line);
+	printf("REQUEST: %s\n", line);
 	if (startsWith("GET /frame.jpg", line))
 	{
-		printf("FRAME\n");
-
 		unsigned char* frame;
 		int length = 0;
 		if (!camFrame(&frame, &length))
@@ -116,7 +91,6 @@ void process(int conn) {
 	}
 	else if (startsWith("GET / ", line))
 	{
-		printf("PAGE\n");
 		sprintf(buf, "HTTP/1.1 200 OK\r\n");
 		sprintf(buf + strlen(buf), "Content-Type: text/html\r\n");
 		// sprintf(buf + strlen(buf), "Content-Length: %i\r\n", length);
@@ -126,7 +100,6 @@ void process(int conn) {
 	}
 	else
 	{
-		printf("NOT FOUND\n");
 		sprintf(buf, "HTTP/1.1 404 Not Found\r\n");
 		sprintf(buf + strlen(buf), "\r\n");
 		write(conn, buf, strlen(buf));
@@ -157,10 +130,8 @@ int main(int argc, char** argv)
     while(1)
 	{
         int conn = serverAccept(fd);
-		printf("ACCEPTED CONNECTION: %i\n", conn);
 		process(conn);
 		serverClose(conn);
-		printf("CLOSED\n");
     }
 
     return 0;

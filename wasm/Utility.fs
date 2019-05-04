0f8f0904767a2891db5647d4eb5a7efd220024cc
AshleyF <ashley.feniello@live.com>
2019-05-03 19:01:05 -0700

    Moved to separate repo 'BriefWasm'

--- wasm/Utility.fs
+++
@@ -1,9 +0,0 @@
-﻿module Utility
-
-open System
-open System.IO
-
-let save (wasm: byte seq) =
-    let base64 = wasm |> Array.ofSeq |> Convert.ToBase64String
-    use writer = File.CreateText("./main.js")
-    writer.WriteLine(sprintf "run('%s');" base64)
